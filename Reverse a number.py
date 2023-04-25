b220c6e63174fb570b0343d2fdaa6538e1b70066
Ankita Ghosh <91538328+Phoenixcoder-6@users.noreply.github.com>
2023-04-25 12:31:00 +0530

    Create Reverse a number.py

---
+++ Reverse a number.py
@@ -0,0 +1,23 @@
+''' This is a Python Program to reverse a given number.
+
+Problem Description
+The program takes a number and reverses it.
+
+Problem Solution
+1. Take the value of the integer and store in a variable.
+2. Using a while loop, get each digit of the number and store the reversed number in another variable.
+3. Print the reverse of the number.
+4. Exit. '''
+
+#SOURCE CODE
+
+n=int(input("Enter a number:"))
+rev=0
+temp=n
+while(n>0):
+    dig= n%10
+    rev=rev*10 + dig
+    n=n//10
+
+print("The reverse of this number is" , rev)
+
