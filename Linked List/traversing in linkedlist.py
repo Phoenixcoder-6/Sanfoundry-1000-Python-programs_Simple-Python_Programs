77b83e4171d61de0c0a869bfb13276b44eed8e40
Ankita Ghosh <91538328+Phoenixcoder-6@users.noreply.github.com>
2024-09-02 21:33:54 +0530

    Add files via upload

---
+++ Linked List/traversing in linkedlist.py
@@ -0,0 +1,18 @@
+class node:
+    def __init__(self,data):
+        self.data= data
+        self.next= None
+
+node1= node(100)
+node2= node(200)
+node3= node(300)
+
+node1.next= node2
+node2.next= node3
+
+head=node1
+current = head
+while current is not None:
+    print(current.data, end="->")
+    current = current.next
+print("None")   
